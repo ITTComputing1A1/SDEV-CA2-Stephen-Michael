77af258340e108284c7cf723ecd23c9e60a8bd26
Grizzly232 <michael.brady.ie@gmail.com>
2016-11-04 13:55:02 +0000

    Add files via upload

--- CA2/CA2/src/ca2/CA2.java
+++ CA2/CA2/src/ca2/CA2.java
@@ -1,21 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca2;
-
 /**
- *
- * @author X00136103
+ * Michael Brady X00136103
  */
+import java.util.Scanner;
 public class CA2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your username");
+        String username;
+        username = input.nextLine();
+        String password;
+        System.out.println("Enter your password");
+        password = input.nextLine();
+        System.out.println("Confirm your password");
+        if(input.nextLine().equals(password)) {
+            System.out.println("Password accepted");
+        }
+        else {
+            System.out.println("Username or Password incorrect");
+        }
+        
+        int selection = 0;
+        
+        while(selection != 5){
+            System.out.printf("Student Finance System%n"
+                    + "1. Calculate Student Grant%n"
+                    + "2. Current Overall Student Average%n"
+                    + "3. Fee Data%n"
+                    + "4. Grant Category Information%n"
+                    + "5. Exit");
+            System.out.println("Select option");
+            selection = input.nextInt();
+            
+            switch(selection){
+                case 1:
+                    System.out.println("You have selected "+"Calculate Student Grant");
+                    String studentName;
+                    String studentNumber;
+                    System.out.println("Enter your name");
+                    input.nextLine();
+                    studentName = input.nextLine();
+                    System.out.println("Enter your student number.");
+                    studentNumber = input.nextLine();
+                    while(studentNumber.charAt(0) != 'X'){
+                        System.out.println("Invalid Student Number format. Number must begin with uppercase 'X'. Please re-enter.");
+                        studentNumber = input.nextLine();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Option 2");
+                    break;
+                case 3:
+                    System.out.println("Option 3");
+                    break;
+                case 4:
+                    System.out.println("Option 4");
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid selection. Please re-enter");
+                    break;
+            }
+        }
     }
-    
 }
